909bdab099a26b0f54421e124f171a33e616436c
Mark Tibbett <mtibbett67@gmail.com>
2023-04-07 01:02:51 -0600

    Updates

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cp ./tmux.conf ~/.tmux.conf
+cp ./.tmux.conf ~/.tmux.conf
 echo "To install the plugin manager:"
 echo "Step 1) git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
 echo "Step 2) Reload tmux if it's already started with leader + r "
