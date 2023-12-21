6e8dd74bda33f51ba6978673b2461fdaa7627b8d
Mark Tibbett <mtibbett67@gmail.com>
2023-12-21 14:12:23 -0700

    Added comments to remap.lua

--- neovim/lua/user/remap.lua
+++ neovim/lua/user/remap.lua
@@ -1,56 +1,54 @@
--- Remap Esc to jk
-vim.keymap.set({"i", "v"}, "jk", "<Esc>", options)
-
+-- Remap Leader keymap
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Remap Esc to jk
+vim.keymap.set({ "i", "v" }, "jk", "<Esc>", options)
+
+-- Set J and K to move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Append line below to current line
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Move down/up file keeping cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Next/Previous find while keeping cursor centered
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
 
 -- turn on spelling checker
 vim.keymap.set("n", "<leader>sp", "<cmd>set spell!<CR>")
 
--- greatest remap ever
+-- Preserves paste buffer so you can past multiple times
+-- "greatest remap ever"
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- "y" yank to system clipboard and "Y" to nvim only
+-- "next greatest remap ever : asbjornHaland"
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- Deletes to void register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- Fixes issue with Ctrl+c in vertical edit mode
+-- "This is going to get me cancelled"
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Stops from accidentally running a macro
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
---vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Starts search/replace with the word you are currently on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Sets the current open file to executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/user/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
-
