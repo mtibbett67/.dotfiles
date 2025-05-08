41a2dd3934be543b8237d97f843e6c42ba51a213
Mark Tibbett <mtibbett67@gmail.com>
2025-05-07 19:28:09 -0600

    Fixing neovim

--- neovim/.config/nvim/lua/vim-options.lua
+++ neovim/.config/nvim/lua/vim-options.lua
@@ -11,9 +11,9 @@ vim.opt.scrolloff = 8
 vim.opt.colorcolumn = "80"
 vim.opt.signcolumn = "yes"
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
