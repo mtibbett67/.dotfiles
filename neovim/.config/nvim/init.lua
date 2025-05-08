bcf6b0aaa6f61dc0c3f9ed18327c733fe8b1f489
Mark Tibbett <mtibbett67@gmail.com>
2025-05-07 21:56:21 -0600

    Restructured all neovim plugins

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,7 +1,26 @@
--- require("user.remap")
 require("vim-options")
-require("remap")
+require("vim-remap")
 
+-- set neovim background with Nightfly to tranparent
+vim.g.nightflyTransparent = true
+
+-- Setup Lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+--[[
 local augroup = vim.api.nvim_create_augroup
 
 local userGroup = augroup('user', {})
@@ -30,36 +49,4 @@ autocmd('TextYankPost', {
 --     pattern = "*",
 --     command = [[%s/\s\+$//e]]
 -- })
-
--- set neovim background with Nightfly to tranparent
---vim.g.nightflyTransparent = true
 --]]
-
--- Setup Lazy package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>' )
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
-
--- neovim/.config/nvim/lua/vim-remap.lua
+++ neovim/.config/nvim/lua/vim-remap.lua
@@ -43,6 +43,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Stops from accidentally running a macro
 vim.keymap.set("n", "Q", "<nop>")
+
+-- LSP format
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Starts search/replace with the word you are currently on
@@ -52,7 +54,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
 
 -- My macro keys
